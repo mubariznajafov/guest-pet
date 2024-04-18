5ba9a4e4127b62b375f98f1505a5450e4715c36f
mubariznacafov <mubariz.necefov.17@gmail.com>
2024-04-18 12:21:52 +0400

    sign-in styling

--- src/pages/sign-in/addingName.jsx
+++ src/pages/sign-in/addingName.jsx
@@ -5,23 +5,27 @@ const addingName = () => {
   return (
     <div className="addingName">
       <div className="top_informations">
-        <h1>Welcome to GuestPet</h1>
+        <h1>Welcome to Guest Pet</h1>
         <p>
           Whether you need to report a lost or found pet or want to register
           your pet in case they go missing later - you’re in the right place.
         </p>
       </div>
-      <form action="">
-        <div className="inputs">
-          <div className="input">
-            <input type="text" id="firstName" placeholder="First Name" />
+      <div className="form">
+        <form action="">
+          <div className="inputs">
+            <div className="input">
+              <input type="text" id="firstName" placeholder="First Name" />
+            </div>
+            <div className="input">
+              <input type="text" id="lastName" placeholder="Last Name" />
+            </div>
           </div>
-          <div className="input">
-            <input type="text" id="lastName" placeholder="Last Name" />
+          <div className="btn">
+            <button>Continue</button>
           </div>
-        </div>
-        <button>Continue</button>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
